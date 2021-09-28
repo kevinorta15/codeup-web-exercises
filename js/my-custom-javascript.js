dbab44e78c7788586702b8fb4d867b4d05325d19
Kevin Orta <orta.kevin@gmail.com>
2021-09-28 15:02:25 -0500

    added hover, click, dblclick functionality to page using jquery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -6,7 +6,23 @@ $(document).ready(function() {
     // INSERT JAVASCRIPT CODE SAMPLE HERE
 });
 
-$('.codeup').css('border', '1px red solid')
-$('li').css('font-size', '20px')
-$('li, h1, p').css('color', 'green')
-var contents = $('#brogramming').html(); alert(contents)
+// $('.codeup').css('border', '1px red solid')
+// $('li').css('font-size', '20px')
+// $('li, h1, p').css('color', 'green')
+// var contents = $('#brogramming').html(); alert(contents)
+
+$("h1, h2").click( function (){
+    $(this).css('background-color', 'cyan')
+});
+
+$('p').dblclick( function (){
+    $('p').css('font-size','18px').css('color', 'red')
+});
+
+$('li').hover( function (){
+    $('li').css('background-color', 'red')
+},
+
+function (){
+    $('li').css('background-color', 'inherit')
+});
