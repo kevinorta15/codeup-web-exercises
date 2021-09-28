dd8bd21f96524a701238559fd266879facda633c
Kevin Orta <orta.kevin@gmail.com>
2021-09-28 12:25:22 -0500

    added stylign using jquery

---
+++ js/my-custom-javascript.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function() {
+
+    // alert("The Document is ready, we are ready to rock and roll! ")
+    // INSERT JAVASCRIPT CODE SAMPLE HERE
+});
+
+$('.codeup').css('border', '1px red solid')
+$('li').css('font-size', '20px')
+$('li, h1, p').css('color', 'green')
+var contents = $('#brogramming').html(); alert(contents)
